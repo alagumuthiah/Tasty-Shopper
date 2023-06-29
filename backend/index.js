071b7e6671fc7b4db8233a3f054a40765bd106a7
alagumuthiah <alagudevi1996@gmail.com>
2023-06-28 22:04:42 -0700

    Created Model, Migration and seeder files for User

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,11 @@ import ingredientRoute from './routes/api/ingredientRouter';
 import recipeRoute from './routes/api/recipeRouter';
 import userRoute from './routes/api/users';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 app.use(cookieParser());
+app.use(cors());
 app.use('/recipes', recipeRoute);
 app.use('/ingredients', ingredientRoute);
 app.use('/users', userRoute);
