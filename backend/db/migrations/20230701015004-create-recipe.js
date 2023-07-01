efe4e7e159c338874be67c85a3afcba93c7520b2
alagumuthiah <alagudevi1996@gmail.com>
2023-06-30 18:52:50 -0700

    Created models and migration for recipe and ingredient

---
+++ backend/db/migrations/20230701015004-create-recipe.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('recipes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      cuisine: {
+        type: Sequelize.STRING
+      },
+      servings: {
+        type: Sequelize.INTEGER
+      },
+      isPublic: {
+        type: Sequelize.BOOLEAN
+      },
+      instructions: {
+        type: Sequelize.ARRAY(Sequelize.STRING)
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('recipes');
+  }
+};
